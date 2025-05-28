6b3595b5ddf7c30be4343d4660d7f68ab4e93691
Litvinov Ilya <dreameprogrammer82@gmail.com>
2025-05-28 15:27:41 +0300

    Отобразить фотографии других пользователей.

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -11,10 +11,13 @@ export const renderThumbnails = () => {
   photoDescriptions.forEach(({url, description, likes, comments}) => {
     const thumbnail = templateContent.cloneNode(true);
     const image = thumbnail.querySelector('.picture__img');
+    const contentLikes = thumbnail.querySelector('.picture__likes');
+    const contentCommentsLength = thumbnail.querySelector('.picture__comments');
+
     image.src = url;
     image.alt = description;
-    thumbnail.querySelector('.picture__likes').textContent = likes;
-    thumbnail.querySelector('.picture__comments').textContent = comments.length;
+    contentLikes.textContent = likes;
+    contentCommentsLength.textContent = comments.length;
     photoFragment.appendChild(thumbnail);
   });
 
