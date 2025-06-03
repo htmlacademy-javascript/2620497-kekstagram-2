ef4c306b1a75f6d33897af0e9407ce23e5e26f21
Litvinov Ilya <dreameprogrammer82@gmail.com>
2025-06-03 11:20:30 +0300

    Улучшить пользовательский интерфейс кнопкой «Загрузить ещё».

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,21 +1,30 @@
 import {createPhotos} from './data.js';
 
+export let photoDescriptions = [];
 
 export const renderThumbnails = () => {
   const template = document.querySelector('#picture').content;
   const templateContent = template.querySelector('.picture');
   const pictures = document.querySelector('.pictures');
-  const photoDescriptions = createPhotos();
+  photoDescriptions = createPhotos();
   const photoFragment = document.createDocumentFragment();
 
-  photoDescriptions.forEach(({url, description, likes, comments}) => {
+  photoDescriptions.forEach(({id, url, description, likes, comments}) => {
     const thumbnail = templateContent.cloneNode(true);
-    thumbnail.querySelector('.picture__img').src = url;
-    thumbnail.querySelector('.picture__img').alt = description;
-    thumbnail.querySelector('.picture__likes').textContent = likes;
-    thumbnail.querySelector('.picture__comments').textContent = comments.length;
+    thumbnail.dataset.photoId = id;
+    const image = thumbnail.querySelector('.picture__img');
+    const contentLikes = thumbnail.querySelector('.picture__likes');
+    const contentCommentsLength = thumbnail.querySelector('.picture__comments');
+
+    thumbnail.dataset.photoId = id;
+    image.src = url;
+    image.alt = description;
+    contentLikes.textContent = likes;
+    contentCommentsLength.textContent = comments.length;
     photoFragment.appendChild(thumbnail);
   });
 
   pictures.appendChild(photoFragment);
 };
+
+export const getPhotoDescriptions = () => photoDescriptions;
