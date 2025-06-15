a7e0e835106db46e3e2ef27e81b06984da848cb1
Litvinov Ilya <dreameprogrammer82@gmail.com>
2025-06-15 15:30:04 +0300

    Перламутровые пуговицы

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -2,6 +2,7 @@ export const renderThumbnails = (photoDescriptions) => {
   const template = document.querySelector('#picture').content;
   const templateContent = template.querySelector('.picture');
   const pictures = document.querySelector('.pictures');
+  pictures.querySelectorAll('.picture').forEach((element) => element.remove());
   const photoFragment = document.createDocumentFragment();
 
   photoDescriptions.forEach(({id, url, description, likes, comments}) => {
